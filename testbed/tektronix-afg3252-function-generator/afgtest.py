11581daf4ebd29227d55b365931d392a3cefe390
hepr2018-2 <kaikai581@hotmail.com>
2020-06-18 18:46:36 -0500

    Now the IP to search for the function generator can be specified by a command line argument.

--- testbed/tektronix-afg3252-function-generator/afgtest.py
+++ testbed/tektronix-afg3252-function-generator/afgtest.py
@@ -1,24 +1,33 @@
 from AFG3252 import * 
+
+import argparse
 import time
 import socket
 
-fg = socket.gethostbyname('192.168.1.200')
+if __name__ == '__main__':
+    # Command line argument to use another IP
+    parser = argparse.ArgumentParser()
+    # Default IP address of the function generator assigned by me.
+    parser.add_argument('-i','--ip', help='IP address of the function generator', default='192.168.0.101', type=str)
+    args = parser.parse_args()
 
-print('Function generator on IP {0}...'.format(fg))
-dev = AFG3252(fg)
+    fg = socket.gethostbyname(args.ip)
 
-dev.disableOutput(1)
-dev.disableOutput(2)
+    print('Function generator on IP {0}...'.format(fg))
+    dev = AFG3252(fg)
 
-dev.outputType('pulse')
-dev.outputPolarity(1,'INVerted')
-dev.setFrequency(7000)
-#dev.setVoltage(1, "50mV")
-dev.setVoltageHigh(1,"50mV")
-dev.setVoltageLow(1,"0mV")
-dev.setLeading(1, "4ns")
-dev.setTrailing(1, "55us")
+    dev.disableOutput(1)
+    dev.disableOutput(2)
 
-dev.enableOutput(1)
-time.sleep(2)
-dev.disableOutput(1)
+    dev.outputType('pulse')
+    dev.outputPolarity(1,'INVerted')
+    dev.setFrequency(7000)
+    #dev.setVoltage(1, "50mV")
+    dev.setVoltageHigh(1,"50mV")
+    dev.setVoltageLow(1,"0mV")
+    dev.setLeading(1, "4ns")
+    dev.setTrailing(1, "55us")
+
+    dev.enableOutput(1)
+    time.sleep(2)
+    dev.disableOutput(1)
